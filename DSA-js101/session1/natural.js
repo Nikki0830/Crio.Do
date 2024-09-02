45c4b53b69f30b73f9aff94134c251e2aa14e02e
Nikki0830 <gniki0830@gmail.com>
2024-09-02 18:17:59 +0530

    DSA-js-101

---
+++ DSA-js101/session1/natural.js
@@ -0,0 +1,11 @@
+//print first 100 natuural numbers
+//exaample of while loop
+
+function printNumbers(){
+    let i = 1
+    while(i <= 100){
+        console.log(i)
+        i++
+    }
+}
+printNumbers()
