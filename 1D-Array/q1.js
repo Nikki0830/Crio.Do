541d751d1a28fa9641b40e9c1d6a899679c2f1aa
Nikki0830 <gniki0830@gmail.com>
2024-09-10 19:24:48 +0530

    DSA

---
+++ 1D-Array/q1.js
@@ -0,0 +1,18 @@
+//find maximum value in the array
+
+//brute force approach
+
+let arr = [1,2,4,56,78]//an array
+let max = arr[0]//initializing array[0] value with max
+
+for(let i = 0; i < arr.length; i++){//iterate the loop from 0 to arra.length
+    if(arr[i] > max){//checking if the any of the value is bigger than max or not
+        max = arr[i]//string the suitable value in max
+    }
+}
+console.log(max)//finally printing the max value
+
+//TIME COMPLEXITY ====> 0(n)// as we are running the loop till n
+//SPACE COMPLEXITY ====> 0(1) as we are changing the values in a same array or not taking any additional space
+
+//Note (Here we cannot optimize the code because each element in the array need to be iterate once to find the maximum value)
